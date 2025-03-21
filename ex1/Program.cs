addd6911f51a0064eec8563f6a5ad2e966b5c3e6
Dav1Lo <davi.lourenco0713@gmail.com>
2025-03-21 10:31:47 -0300

    first commit

---
+++ ex1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite um número:");
+int p;
+p = int.Parse(Console.ReadLine());
+if(p%2==0){
+    Console.WriteLine("Esse número é par.");
+}
+else {
+    Console.WriteLine("Esse número é ímpar.");
+}
